2e2fbd171d80302f2b139809694fcf5495d8bc32
Ashwin Singh <ashwin@Ashwins-MacBook-Air.local>
2022-12-01 02:22:10 +0530

    Home page products added

--- shopping-frontend/src/components/home/CardCategory.jsx
+++ shopping-frontend/src/components/home/CardCategory.jsx
@@ -1,17 +1,16 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 
 export default function CardCategory (props) {
-    console.log(props.img)
     return(
         <Card sx={{maxWidth:345}}>
             <CardActionArea>
                 <CardMedia 
                     component='img'
-                    height="200"
+                    height="175"
                     image={props.img}
                 />
                 <CardContent>
-                    <Typography style={{textAlign:'center'}} variant="h5">{props.name}</Typography>
+                    <Typography style={{textAlign:'center'}} variant="h6">{props.name}</Typography>
                 </CardContent>
             </CardActionArea>
         </Card>
